eca01af160b5b0440f38de4f42fc435c32d2f71f
Erin Woodard <79154737+erinwoodard8@users.noreply.github.com>
2021-03-18 21:42:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import Game from './components/Game';
+import './css/index.css';
+
+
+
+
+
+
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+reportWebVitals();
